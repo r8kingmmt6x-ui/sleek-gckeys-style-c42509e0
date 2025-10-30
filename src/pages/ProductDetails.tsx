40c74e2246504a84f08406efa9cf1422ef5aab8d
r8kingmmt6x-ui <r8kingmmt6x@gmail.com>
2025-10-30 19:59:54 +0400

    Update ProductDetails.tsx

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -35,7 +35,7 @@ const products = [
     icon: "",
     slug: "volcano-executor",
     plans: [
-      { name: "Monthly", duration: "30-Day Access", price: "$6.99", inStock: false, productId: 393930, variantId: 562297 },
+      { name: "Monthly", duration: "30-Day Access", price: "$6.99", inStock: false, productId: , variantId:  },
       { name: "Quarterly", duration: "90-Day Access", price: "$14.99", inStock: false, productId: 393930, variantId: 639153 },
       { name: "Lifetime", duration: "Lifetime Access", price: "$26.99", inStock: false, productId: 393930, variantId: 639154 }
     ],

c0dc505b004a4c43edc6323857ecfd1aef93a53c
r8kingmmt6x-ui <r8kingmmt6x@gmail.com>
2025-11-01 11:50:19 +0400

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -26,7 +26,7 @@ const products = [
     price: "$7.99 - $14.99",
     stock: 15,
     image: "https://cdn.imgchest.com/files/9d6fa32a3840.webp",
-    slug: "exploitingis-fun"
+    slug: "exploiting-is-fun"
   },
   {
     title: "Seliware",
